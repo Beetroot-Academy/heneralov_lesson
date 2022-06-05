1011de75e4ad43e72604f62bf252e76cef8e2581
heneralovsem <semm297@gmail.com>
2022-06-05 11:35:25 +0300

    fix

--- Homework18/js/main.js
+++ Homework18/js/main.js
@@ -11,28 +11,28 @@ console.log (result);
 
 
 const fileMbSize = 820;
-let capacityGb = prompt ("Enter the capacity of your flash drive ( in gb)");
+let capacityGb = prompt("Enter the capacity of your flash drive ( in gb)");
 let capacityConverted = capacityGb * 1000;
-let amountOfFiles = capacityConverted / fileMbSize;
-alert ("You can fit " + amountOfFiles.toFixed(0) + " files");
+let fileAmount = capacityConverted / fileMbSize;
+alert ("You can fit " + fileAmount.toFixed(0) + " files");
 
 
 
-let amountOfMoney = prompt ("How much money do you want to spend?");
-let chocoPrice = prompt ("The price of the chocolate you want to buy");
-let amountOfChoco =Math.floor( amountOfMoney / chocoPrice);
-let moneyRest = amountOfMoney % chocoPrice;
-alert ("You can buy " + amountOfChoco + " chocolates and you will have " + moneyRest + "$ rest"); 
+let moneyAmount = prompt("How much money do you want to spend?");
+let chocoPrice = prompt("The price of the chocolate you want to buy");
+let chocoAmount =Math.floor( moneyAmount / chocoPrice);
+let moneyRest = moneyAmount % chocoPrice;
+alert ("You can buy " + chocoAmount + " chocolates and you will have " + moneyRest + "$ rest"); 
 
 
-let userNum = prompt ( "Enter three digit number ");
+let userNum = prompt( "Enter three digit number ");
 let firstNum = userNum % 10;
 let secondNum = (userNum % 100 - firstNum) / 10;
 let thirdNum = (userNum - (userNum % 100 - firstNum) - firstNum ) / 100;
 alert ("Reversed number is " + firstNum + secondNum + thirdNum);
 
 
-let userDeposit = prompt ("How much money do you want to invest?");
+let userDeposit = prompt("How much money do you want to invest?");
 let percent = (0.05 / 12) * 2;
 let moneyReceived = userDeposit * percent;
 alert ("Your income is " +moneyReceived.toFixed(2) + "$");
