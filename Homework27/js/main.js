5bcd4da215e723252d843e6373866004f76b8171
heneralovsem <semm297@gmail.com>
2022-07-17 11:12:01 +0300

    homework 27

---
+++ Homework27/js/main.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide("#hero-splide", {
+    perPage: 1,
+    perMove: 1,
+    type: "loop",
+    autoplay: true,
+    interval: 2500,
+    arrows: false,
+    cover: true,
+    height: "700px",
+    pauseOnHover: false,
+    
+
+    
+  });
+  splide.mount();
+
+
+
+  let secondSplide = new Splide("#arrivals-splide", {
+    perPage: 5,
+    perMove: 1,
+    type: "loop",
+    cover: true,
+    autoplay: true,
+    interval: 2500,
+    pagination: false,
+    pauseOnHover: false,
+    breakpoints: {
+      768: {
+        perPage: 1,
+        arrows: false,
+        pagination: true,
+      },
+      1320: {
+        perPage: 3,
+        gap: '15px',
+        
+      },
+    },
+    
+    
+    
+  });
+  secondSplide.mount();
+
+  let thirdSplide = new Splide("#partners-splide", {
+    perPage: 9,
+    perMove: 1,
+    type: "loop",
+    pagination: false,
+    pauseOnHover: false,
+    direction: 'ltr',
+
+    breakpoints: {
+      768: {
+        perPage: 3,
+        arrows: false,
+        pagination: true,
+      },
+      1320: {
+        perPage: 6,
+        gap: '15px',
+        
+      },
+    },
+    
+  });
+  thirdSplide.mount();
+});
