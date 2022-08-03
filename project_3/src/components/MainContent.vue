86e227d3f430e05f3880d2456cd5ebb40ba243f8
heneralovsem <semm297@gmail.com>
2022-08-03 16:58:38 +0300

    save

--- project_3/src/components/MainContent.vue
+++ project_3/src/components/MainContent.vue
@@ -387,7 +387,7 @@ export default {
         this.page = 1;
         this.filteredSearch = "";
         fetch(
-          `https://thingproxy.freeboard.io/fetch/api/v1/search?q=${this.queryValue}&categories=${this.categories}&resolutions=${this.exactResValue}&sorting=${this.sortValue}&topRange=${this.topSortValue}&order=${this.orderValue}&page=${this.page}`
+          `/api/v1/search?q=${this.queryValue}&categories=${this.categories}&resolutions=${this.exactResValue}&sorting=${this.sortValue}&topRange=${this.topSortValue}&order=${this.orderValue}&page=${this.page}`
         )
           .then((res) => res.json())
           .then((json) => {
