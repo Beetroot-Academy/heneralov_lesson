f2b751359845a3453cabf530f0da9b7bde2be698
heneralovsem <semm297@gmail.com>
2022-09-14 21:58:46 +0300

    test

--- film_project/src/App.vue
+++ film_project/src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="search-wrapper">
+          <input
+            type="text"
+            class="search-bar"
+            placeholder="Search..."
+            v-model="query"
+            @keypress.enter="fetchWeather"
+          />
+        </div>
+        {{query}}
+        <div class="weather-wrapper" v-if="test">
+          <div class="location-box">
+            <div class="location">{{weather.city.name}}, {{weather.city.country}}</div>
+            <div class="date">Monday 12 January 1488</div>
+          </div>
+          <div class="weather-box">
+            <div class="temperature">12deg</div>
+            <div class="weather">Rain</div>
+          </div>
+        </div>
+        </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      apikey: "a9be263066308193e2963b50dd2671f3",
+      query: "",
+      weather: [],
+      test: false,
+      
+    }
+  },
+  methods: {
+    fetchWeather() {
+      this.test = true;
+        fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${this.query}&appid=${this.apikey}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          this.weather = data;
+            console.log(this.weather);
+          })
+          .finally(() => (this.isLoading = false));
+      }
+
   }
 }
 </script>
